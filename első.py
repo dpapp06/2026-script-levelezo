49a2e8d323d42ec0c1e4ff11a361d60db67cae36
dpapp06 <dpapp290@gmail.com>
2026-09-25 11:48:42 +0200

    első szinkron

--- első.py
+++ első.py
@@ -1,1 +1,7 @@
-print('Szia')
+#print('Szia')
+
+felhasználó_neve='Dániel'
+felhasználó_kora=int(input("Hány éves vagy?:"))
+felhasználó_kora/=2
+
+print('Szia', felhasználó_neve,'!', felhasználó_kora)
